3e27cd10674624e511dc1bbd6449ac47b60da217
Joe Boylson <joeboylson@gmail.com>
2024-10-18 17:36:31 -0400

    debugging frontend env variables;

--- client/src/utils/sanity.ts
+++ client/src/utils/sanity.ts
@@ -2,14 +2,20 @@ import urlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/asset-utils";
 import { createClient, type ClientConfig } from "@sanity/client";
 
-const sanityConfig: ClientConfig = {
-  projectId: process.env.REACT_APP_ID,
-  dataset: process.env.REACT_APP_DATASET,
-  apiVersion: process.env.REACT_APP_API_VERSION,
-  useCdn: true,
+const getSanityClient = () => {
+  console.log(process.env);
+
+  const sanityConfig: ClientConfig = {
+    projectId: process.env.REACT_APP_ID,
+    dataset: process.env.REACT_APP_DATASET,
+    apiVersion: process.env.REACT_APP_API_VERSION,
+    useCdn: true,
+  };
+
+  return createClient(sanityConfig);
 };
 
-export const sanityClient = createClient(sanityConfig);
+export const sanityClient = getSanityClient();
 
 export const makeImageUrl = (sanityImageSource: SanityImageSource) => {
   return urlBuilder(sanityClient)
